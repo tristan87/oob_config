abb8bac9206f244484c1bc24ad65b98e1fa22c84
Tristan Davis <tristan.seabrook@gmail.com>
2020-05-08 17:50:24 -0600

    fixed bug with logname module

--- app/logname.js
+++ app/logname.js
@@ -2,12 +2,18 @@
 
 //create a filename based on the current date and time
 module.exports = (ext) => {
+  let zeroPad = (timeObj) => {
+    let timeStr = timeObj.toString();
+    return timeStr.padStart(2, '0');
+  };
+
   let date =    new Date();
-  let year =    date.getFullYear();
-  let month =   date.getMonth().toString().padStart(2, '0');
-  let day =     date.getDay().toString().padStart(2, '0');
-  let hour =    date.getHours().toString().padStart(2, '0');
-  let minute =  date.getMinutes().toString().padStart(2, '0');
-  let second =  date.getSeconds().toString().padStart(2, '0');
+
+  let year =    zeroPad(date.getFullYear());
+  let month =   zeroPad(date.getMonth() + 1);
+  let day =     zeroPad(date.getDate());
+  let hour =    zeroPad(date.getHours());
+  let minute =  zeroPad(date.getMinutes());
+  let second =  zeroPad(date.getSeconds());
   return `${year}-${month}-${day}_${hour}-${minute}-${second}.${ext}`;
 };
