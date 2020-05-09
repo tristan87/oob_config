6850ce549cba63ec007ce5134ed182dfeea1cc90
Tristan Davis <tristan.seabrook@gmail.com>
2020-05-08 19:58:11 -0600

    remove reset command from ilo commands array

--- app/ilo.js
+++ app/ilo.js
@@ -8,8 +8,7 @@ module.exports = (config) => {
       `set /map1/dnsendpt1 EnabledState=yes Hostname=${config.formattedHostname}`,
       `set /map1/enetport1/lanendpt1/ipendpt1 IPv4Address=${config.currentIP} SubnetMask=${config.netmask}`,
       `set /map1/gateway1 AccessInfo=${config.gateway}`,
-      `create /map1/accounts1/ username=${config.setUsername} password=${config.setPassword} group=admin,config,oemHPE_rc,oemHPE_power,oemHPE_vm`,
-      `reset /map1`
+      `create /map1/accounts1/ username=${config.setUsername} password=${config.setPassword} group=admin,config,oemHPE_rc,oemHPE_power,oemHPE_vm`
     ],
     successRegex: /status_tag=COMMAND COMPLETED/g,
     commandRegex: />hpiLO->set /g,
