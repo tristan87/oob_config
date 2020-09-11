e6768334f05cff3e03b03b9cd6471894f1d1ecc0
Tristan Davis <tristan.seabrook@gmail.com>
2020-09-11 07:48:17 -0600

    changed iLO config settings

--- app/ilo.js
+++ app/ilo.js
@@ -7,7 +7,7 @@ module.exports = (config) => {
       `set /map1/enetport1/lanendpt1/ipendpt1 IPv4Address=${config.currentIP} SubnetMask=${config.netmask}`,
       `set /map1/gateway1 AccessInfo=${config.gateway}`,
       `set /map1/settings1/WINSSettingData1 RegisterThisConnection=no`,
-      `set /map1/accounts1/Administrator username=${config.setUsername} password=${config.setPassword}`
+      `create /map1/accounts1/ username=${config.setUsername} password=${config.setPassword}`
     ],
     successRegex: /status_tag=COMMAND COMPLETED/g,
     commandRegex: />hpiLO->set /g,
